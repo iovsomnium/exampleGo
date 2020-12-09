8041af76eaa4fcc5ebba998b287353756010be9f
Ui Hyeon Lee <ksw09157.5212@gmail.com>
2020-12-09 09:32:32 +0900

    first add

---
+++ main.go
@@ -0,0 +1,21 @@
+// 컴파일을 할때는 main파일이 필요하다
+package main
+
+import (
+	"fmt"
+)
+
+//formating을 위한 librarly
+
+// func repeatMe(words ...string) {
+// 	fmt.Println(words)
+// }
+
+func main() {
+	// name := "winner" //==const name string = 'nico'
+	// repeatMe("king", "winner", "func", "kong")
+	defer fmt.Println("i'm done")
+	// *b = 20 //pointer 원본 값 변경
+	// a = 10
+
+}
