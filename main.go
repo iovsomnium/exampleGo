bb925a1567e907cc5c0bdce62107926ae2dfeda1
Ui Hyeon Lee <ksw09157.5212@gmail.com>
2020-12-17 20:39:26 +0900

    learn channel

--- main.go
+++ main.go
@@ -64,13 +64,13 @@ func main() {
 	for _, person := range people {
 		go isReal(person, c)
 	}
-	result := <-c
-	fmt.Println(result)
-
+	fmt.Println(<-c)
+	fmt.Println(<-c)
 }
 
-func isReal(peson string, c chan bool) {
+func isReal(person string, c chan bool) {
 	time.Sleep(time.Second * 5)
+	fmt.Println(person)
 	c <- true
 }
 
